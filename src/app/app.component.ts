c6e1a851eea44aa2856eaa77d6c7f9061686a39a
MUK <mukxdev@gmail.com>
2023-10-31 17:39:27 +0300

    animation on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LanguagesService } from './services/languages.service';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(public language: LanguagesService) {}
+  ngOnInit(): void {
+    AOS.init({ once: true });
+  }
 
   title = 'SNIC_1399';
 }
